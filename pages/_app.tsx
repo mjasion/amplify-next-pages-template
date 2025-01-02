926fd6f36768b845c4dd8750107e246f6b099f80
Marcin Jasion <mjasion@users.noreply.github.com>
2025-01-02 09:56:40 +0100

    drop authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,11 @@
 import "@/styles/app.css";
-import type {AppProps} from "next/app";
-import {Amplify} from "aws-amplify";
+import type { AppProps } from "next/app";
+import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-import {Authenticator} from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
-
 Amplify.configure(outputs);
 
-export default function App({Component, pageProps}: AppProps) {
-    return (
-        <Authenticator>
-            <Component {...pageProps} />;
-        </Authenticator>
-    )
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
